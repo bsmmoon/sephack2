5e9b15ed749eb651592ded2235e776327b6be9f4
bsmmoon <bsmmoon@gmail.com>
2018-07-11 15:26:05 +0800

    basic features. my ui sucks

--- src/App.js
+++ src/App.js
@@ -14,8 +14,13 @@ class App extends Component {
     this.full = <i className="fas fa-circle" style={{'fontSize': '30px'}}></i>
 
     this.state = {
-      total: 3,
+      steps: [
+        {title: 'Simple Link', message: 'Welcome', type: 'link', data: {link: 'https://www.sephora.sg/'}},
+        {title: 'G Form', message: 'Terms and Conditions applied', type: 'link', data: {link: 'https://docs.google.com/forms/d/e/1FAIpQLSczb52p39n4xvaEFFkKBcF9AJ63m0B5TbkCLDSnRdWPCP5BUQ/viewform?embedded=true'}},
+        {title: 'Image', message: 'WOW', type: 'image', data: {link: 'https://i.kym-cdn.com/entries/icons/mobile/000/013/564/doge.jpg'}},
+      ],
       progress: 0,
+      complete: false,
     }
   }
 
@@ -32,61 +37,124 @@ class App extends Component {
   headerComponent(header, subheader) {
     return (
       <div className="row">
-        <div className="col-xs-12" style={Style.merge([this.style.base.align.hcvc, {color: 'white', background: 'black'}])}>
-          <h1 style={{margin: '20px'}}>{header}</h1>
+        <div className="col-xs-12" style={Style.merge([this.style.base.align.hcvc,
+          {
+            color: 'white',
+            background: 'black',
+            'font-weight': '700',
+            'letter-spacing': '0.5px',
+            'font-size': '24px',
+            'line-height': '26px',
+          }])}>
+          <h1 style={{margin: '20px 0px 10px 0px'}}>{header}</h1>
         </div>
-        <div className="col-xs-12" style={this.style.base.align.hcvc}>
-          <h2>{subheader}</h2>
+        <div className="col-xs-12" style={Style.merge([this.style.base.align.hcvc, {color: 'white', background: 'black'}])}>
+        <p style={{margin: '10px 0px 20px 0px'}}>{subheader}</p>
         </div>
       </div>
     )
   }
 
+  makeCurrentStepComponent() {
+    let step = this.state.steps[this.state.progress]
+    let action = '';
+    switch (step.type) {
+      case 'link':
+        action = <a href={step.data.link}>LINK</a>;
+        break;
+      case 'image':
+        action = <img src={step.data.link}/>
+      default:
+        break;
+    }
+    return (
+      <div>
+        <div className="col-xs-12" style={this.style.base.align.hcvc}>
+          <h3>{step.title}</h3>
+        </div>
+        <div className="col-xs-12" style={this.style.base.align.hcvc}>
+          {step.message}
+        </div>
+        <div className="col-xs-12" style={this.style.base.align.hcvc}>
+          {action}
+        </div>
+      </div>
+    )
+  }
+
+  progress() {
+    let progress = this.state.progress + 1;
+    if (!(progress < this.state.steps.length)) {
+      this.setState({complete: true})
+    } else {
+      this.setState({progress: progress});
+    }
+  }
+
   progressComponent() {
     let progress = [];
-    for (let i = 0; i < this.state.total; i++) {
-      progress.push(<span key={`${i}-bar`}>{this.bar}</span>)
+    for (let i = 0; i < this.state.steps.length; i++) {
       if (i <= this.state.progress) {
+        progress.push(<span key={`${i}-bar`}>{this.bar}</span>)
         progress.push(<span key={`${i}-circle`}>{this.full}</span>)
       } else {
-        progress.push(<span key={`${i}-circle`}>{this.empty}</span>)
+        progress.push(<span key={`${i}-bar`} style={{color: 'grey'}}>{this.bar}</span>)
+        progress.push(<span key={`${i}-circle`} style={{color: 'grey'}}>{this.empty}</span>)
       }
     }
-    progress.push(<span key='end-bar'>{this.bar}</span>)
+    if (this.state.complete) {
+      progress.push(<span key='end-bar'>{this.bar}</span>)
+    } else {
+      progress.push(<span key='end-bar' style={{color: 'grey'}}>{this.bar}</span>)      
+    }
     
     return (
       <div className="row">
-        <div className="col-xs-12" style={Style.merge([this.style.base.align.hcvc])}>
-          {progress}
+        <div className="col-xs-12" style={Style.merge([this.style.base.align.hcvc, {padding: '20px'}])}>
+          <div style={this.style.base.align.hcvc}>
+            {progress}
+          </div>
         </div>
-        <div className="col-xs-12" style={this.style.base.align.hcvc}>
-          STEP {this.state.progress + 1}
-        </div>
-        <div className="col-xs-12" style={this.style.base.align.hcvc}>
-          <h3>Sign Up to Sephora</h3>
-        </div>
-        <div className="col-xs-12" style={this.style.base.align.hcvc}>
-          <a href="https://google.com">LINK</a>
-        </div>
-        <div className="col-xs-12" style={this.style.base.align.hcvc}>
-          Do this and that
-        </div>
-        <div className="col-xs-12" style={Style.merge([this.style.base.align.hcvc, this.style.base.cursor.pointer, this.style.base.unselectable])} onClick={() => {this.setState({progress: this.state.progress + 1})}}>
+        <div className="col-xs-4"></div>
+        <button className="col-xs-4" style={Style.merge([this.style.base.align.hcvc,
+          {
+            border: '0px',
+            padding: '5px',
+            'color': '#fff',
+            'background-color': '#d50032',
+            'border-color': '#d50032',
+          }])} onClick={this.progress.bind(this)}>
           NEXT
+        </button>
+        <div className="col-xs-4"></div>
+        {this.makeCurrentStepComponent()}
+      </div>
+    )
+  }
+
+  completeComponent() {
+    return (
+      <div className="row">
+        <div className="col-xs-12" style={Style.merge([this.style.base.align.hcvc, {padding: '20px'}])}>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwzrgO9g90pZIK4vHItL08PILDUATpj16B328szTdFGGBNzuLN"/>
+        </div>
+        <div className="col-xs-12" style={this.style.base.align.hcvc}>
+          Thank you for completing the survey.
         </div>
       </div>
     )
   }
 
   render () {
+    console.log(this.state)
     const url = 'http://u-wantitblog.com/wp-content/uploads/2017/03/sephora-header.jpg';
     const header = 'SEPHORA'
     const subheader = 'new minion guide'
     return (
-      <div className="container" style={{width: '900px'}}>
+      <div className="container" style={{width: '900px', fontFamily: '"Avalon", CenturyGothic, Helvetica, Arial'}}>
         {this.bannerComponent(url)}
         {this.headerComponent(header, subheader)}
-        {this.progressComponent()}
+        {this.state.complete ? this.completeComponent() : this.progressComponent()}
       </div>
     )
   }
