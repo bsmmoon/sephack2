62fd76c6ad9c6dcb5d166794e3ff48d2f37d6813
bsmmoon <bsmmoon@gmail.com>
2018-07-11 19:25:26 +0800

    make data look cleaner

--- src/App.js
+++ src/App.js
@@ -26,60 +26,72 @@ class App extends Component {
       slidesToScroll: 1
     };
     
+    let data = {};
     const id = this.getParameterByName('id', window.location);
+    
     if (id == 2) {
-      this.banner = 'http://u-wantitblog.com/wp-content/uploads/2017/03/sephora-header.jpg';
-      this.header = 'Admin Account Creation'
-      this.subheader = 'step by step'
-      this.headerColor = 'white'
-      this.headerBackground = 'black'
-
-      this.steps = [
-        {title: 'Step 1', message: 'Select register on the top left corner and click ‘I’M NEW TO SEPHORA’', type: 'image', data: {
-          link: 'https://i.imgur.com/Te5DsXA.png', caption: '',
-        }},
-        {title: 'Step 2', message: 'Input your details and your staff email address accordingly and continue.', type: 'image', data: {
-          link: 'https://i.imgur.com/LmGgCng.png', caption: '',
-        }},
-      ]
+      data = {
+        banner: 'http://u-wantitblog.com/wp-content/uploads/2017/03/sephora-header.jpg',
+        header: 'Admin Account Creation',
+        subheader: 'step by step',
+        headerColor: 'white',
+        headerBackground: 'black',
+        steps: [
+          {title: 'Step 1', message: 'Select register on the top left corner and click ‘I’M NEW TO SEPHORA’', type: 'image', data: {
+            link: 'https://i.imgur.com/Te5DsXA.png', caption: '',
+          }},
+          {title: 'Step 2', message: 'Input your details and your staff email address accordingly and continue.', type: 'image', data: {
+            link: 'https://i.imgur.com/LmGgCng.png', caption: '',
+          }},
+        ]
+      }
     } else if (id == 3) {
-      this.banner = 'https://i.imgur.com/ScAasNC.png';
-      this.header = 'How to Make Omelette'
-      this.subheader = 'for the hungry souls'
-      this.headerColor = 'black'
-      this.headerBackground = '#FFE4B5'
-
-      this.steps = [
-        {title: 'Step 1', message: 'BEAT eggs, water, salt and pepper in small bowl until blended.', type: 'image', data: {
-          link: 'https://x9wsr1khhgk5pxnq1f1r8kye-wpengine.netdna-ssl.com/wp-content/uploads/Step-1-2-120x120.jpg', caption: '',
-        }},
-        {title: 'Step 2', message: 'HEAT butter in 7 to 10-inch nonstick omelet pan or skillet over medium-high heat until hot. TILT pan to coat bottom. POUR IN egg mixture. Mixture should set immediately at edges.', type: 'image', data: {
-          link: 'https://x9wsr1khhgk5pxnq1f1r8kye-wpengine.netdna-ssl.com/wp-content/uploads/Step-2-3-120x120.jpg', caption: '',
-        }},
-        {title: 'Step 3', message: 'GENTLY PUSH cooked portions from edges toward the center with inverted turner so that uncooked eggs can reach the hot pan surface. CONTINUE cooking, tilting pan and gently moving cooked portions as needed.', type: 'image', data: {
-          link: 'https://x9wsr1khhgk5pxnq1f1r8kye-wpengine.netdna-ssl.com/wp-content/uploads/Step-3-3-120x120.jpg', caption: '',
-        }},
-        {title: 'Step 4', message: 'When top surface of eggs is thickened and no visible liquid egg remains, PLACE filling on one side of the omelet. FOLD omelet in half with turner. With a quick flip of the wrist, turn pan and INVERT or SLIDE omelet onto plate. SERVE immediately.', type: 'image', data: {
-          link: 'https://x9wsr1khhgk5pxnq1f1r8kye-wpengine.netdna-ssl.com/wp-content/uploads/Step-4-120x120.jpg', caption: '',
-        }},
-      ]
+      data = {
+        banner: 'https://i.imgur.com/ScAasNC.png',
+        header: 'How to Make Omelette',
+        subheader: 'for the hungry souls',
+        headerColor: 'black',
+        headerBackground: '#FFE4B5',
+        steps: [
+          {title: 'Step 1', message: 'BEAT eggs, water, salt and pepper in small bowl until blended.', type: 'image', data: {
+            link: 'https://x9wsr1khhgk5pxnq1f1r8kye-wpengine.netdna-ssl.com/wp-content/uploads/Step-1-2-120x120.jpg', caption: '',
+          }},
+          {title: 'Step 2', message: 'HEAT butter in 7 to 10-inch nonstick omelet pan or skillet over medium-high heat until hot. TILT pan to coat bottom. POUR IN egg mixture. Mixture should set immediately at edges.', type: 'image', data: {
+            link: 'https://x9wsr1khhgk5pxnq1f1r8kye-wpengine.netdna-ssl.com/wp-content/uploads/Step-2-3-120x120.jpg', caption: '',
+          }},
+          {title: 'Step 3', message: 'GENTLY PUSH cooked portions from edges toward the center with inverted turner so that uncooked eggs can reach the hot pan surface. CONTINUE cooking, tilting pan and gently moving cooked portions as needed.', type: 'image', data: {
+            link: 'https://x9wsr1khhgk5pxnq1f1r8kye-wpengine.netdna-ssl.com/wp-content/uploads/Step-3-3-120x120.jpg', caption: '',
+          }},
+          {title: 'Step 4', message: 'When top surface of eggs is thickened and no visible liquid egg remains, PLACE filling on one side of the omelet. FOLD omelet in half with turner. With a quick flip of the wrist, turn pan and INVERT or SLIDE omelet onto plate. SERVE immediately.', type: 'image', data: {
+            link: 'https://x9wsr1khhgk5pxnq1f1r8kye-wpengine.netdna-ssl.com/wp-content/uploads/Step-4-120x120.jpg', caption: '',
+          }},
+        ]
+      }
     } else {
-      this.banner = 'http://u-wantitblog.com/wp-content/uploads/2017/03/sephora-header.jpg';
-      this.header = 'New Hire Guide'
-      this.subheader = 'welcome'
-      this.headerColor = 'white'
-      this.headerBackground = 'black'
-
-      this.steps = [
-        {title: 'Sign Up to Sephora.sg', message: 'Ask your neighbor for admin access', type: 'link', data: {link: 'https://www.sephora.sg/'}},
-        {title: 'Personal Details Survey', message: 'Your data is safe with us', type: 'google-form', data: {
-          link: 'https://docs.google.com/forms/d/e/1FAIpQLSczb52p39n4xvaEFFkKBcF9AJ63m0B5TbkCLDSnRdWPCP5BUQ/viewform?embedded=true',
-          api: 'https://script.google.com/macros/s/AKfycbwrXYiq_0bLfdfePko1TfWrB4m5D70KTU3XIuX3MuXeCyfrS7o/exec',
-        }},
-        {title: 'Freshdesk', message: 'Ask your neighbor for access', type: 'link', data: {link: 'https://luxola.freshdesk.com/helpdesk'}},
-        {title: 'How to create an admin account', message: 'Step by step guide', type: 'link', data: {link: '?id=2'}},
-      ]
+      data = {
+        banner: 'http://u-wantitblog.com/wp-content/uploads/2017/03/sephora-header.jpg',
+        header: 'New Hire Guide',
+        subheader: 'welcome',
+        headerColor: 'white',
+        headerBackground: 'black',
+        steps: [
+          {title: 'Sign Up to Sephora.sg', message: 'Ask your neighbor for admin access', type: 'link', data: {link: 'https://www.sephora.sg/'}},
+          {title: 'Personal Details Survey', message: 'Your data is safe with us', type: 'google-form', data: {
+            link: 'https://docs.google.com/forms/d/e/1FAIpQLSczb52p39n4xvaEFFkKBcF9AJ63m0B5TbkCLDSnRdWPCP5BUQ/viewform?embedded=true',
+            api: 'https://script.google.com/macros/s/AKfycbwrXYiq_0bLfdfePko1TfWrB4m5D70KTU3XIuX3MuXeCyfrS7o/exec',
+          }},
+          {title: 'Freshdesk', message: 'Ask your neighbor for access', type: 'link', data: {link: 'https://luxola.freshdesk.com/helpdesk'}},
+          {title: 'How to create an admin account', message: 'Step by step guide', type: 'link', data: {link: '?id=2'}},
+        ]
+      }
     }
+
+    this.banner = data.banner;
+    this.header = data.header;
+    this.subheader = data.subheader;
+    this.headerColor = data.headerColor;
+    this.headerBackground = data.headerBackground;
+    this.steps = data.steps;
 
     this.stepDisplay = <Slider ref={slider => (this.slider = slider)} {...this.slideSettings}>
       {
